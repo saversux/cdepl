7542bc09fa9e52e09498961e2a179659aea2443e
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-11-28 09:14:16 +0100

    cluster: Add API check

--- cluster.sh
+++ cluster.sh
@@ -23,7 +23,7 @@ cdepl_cluster_init()
 	# Include
 	source ${CDEPL_SCRIPT_DIR}/cluster/${cluster_name}
 
-	__cdepl_cluster_check_api
+	__cdepl_cluster_check_cluster_api $cluster_name
 
 	# Call "constructor"
 	_cdepl_cluster_on_init "$cluster_user" "${@:3}"
@@ -50,20 +50,48 @@ cdepl_cluster_app_load()
 
 	# Include
 	source ${CDEPL_SCRIPT_DIR}/app/${cluster_app}
-
-	__cdepl_cluster_check_app_api
 }
 
-__cdepl_cluster_check_api()
+__cdepl_cluster_check_cluster_api()
 {
-	TODO=""
+	local cluster_type=$1
 
-	# TODO check if all functions with correct names are loaded
+	__cdepl_cluster_assert_function cdepl_cluster_node_alloc
+	__cdepl_cluster_assert_function cdepl_cluster_walltime
+	__cdepl_cluster_assert_function cdepl_cluster_node_excl
+	__cdepl_cluster_assert_function cdepl_cluster_node_cpus
+	__cdepl_cluster_assert_function cdepl_cluster_node_mem
+	__cdepl_cluster_assert_function cdepl_cluster_node_network
+	__cdepl_cluster_assert_function cdepl_cluster_resolve_dependency
+	__cdepl_cluster_assert_function cdepl_cluster_resolve_hostname_to_ip
+	__cdepl_cluster_assert_function cdepl_cluster_resolve_node_to_ip
+	__cdepl_cluster_assert_function cdepl_cluster_login_cmd
+	__cdepl_cluster_assert_function cdepl_cluster_send_file_to_login
+	__cdepl_cluster_assert_function cdepl_cluster_node_cmd
+	__cdepl_cluster_assert_function cdepl_cluster_get_alloc_node_count
+	__cdepl_cluster_assert_function cdepl_cluster_node_resolve_node_to_hostname
+
+	__cdepl_cluster_assert_function _cdepl_cluster_on_init
+	__cdepl_cluster_assert_function _cdepl_cluster_on_node_setup_finish
+	__cdepl_cluster_assert_function _cdepl_cluster_on_env_setup_finish
+	__cdepl_cluster_assert_function _cdepl_cluster_before_deploy
+	__cdepl_cluster_assert_function _cdepl_cluster_after_deploy
+	__cdepl_cluster_assert_function _cdepl_cluster_before_cleanup
+	__cdepl_cluster_assert_function _cdepl_cluster_after_cleanup
 }
 
-__cdepl_cluster_check_app_api()
+__cdepl_cluster_assert_function()
 {
-	TODO=""
+	local func=$1
+	local cluster_type=$2
+	
+	local type="$(type -t $func)"
 
-	# TODO check if all functions with correct names are loaded
+	if [ ! "$type" ]; then
+		util_error_and_exit "Missing function $func in cluster type $cluster_type"
+	fi
+
+	if [ ! "$type" != "function" ]; then
+		util_error_and_exit "$func is not a function ($type) in cluster type $cluster_type"
+	fi
 }
