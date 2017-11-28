d359a6d4d2eca83c10fb06df7a23cbeaeabde2e8
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-11-28 13:00:07 +0100

    deploy: Cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-DEPLOY_OUT_PATH=""
-DEPLOY_CUR_OUT_PATH=""
+__DEPLOY_OUT_PATH=""
+__DEPLOY_CUR_OUT_PATH=""
 
 cdepl_deploy_out_path()
 {
 	local path=$1
 	
-	DEPLOY_OUT_PATH="$path"
-	DEPLOY_CUR_OUT_PATH="${DEPLOY_OUT_PATH}/$(date '+%Y-%m-%d_%H-%M-%S-%3N')"
+	__DEPLOY_OUT_PATH="$path"
+	__DEPLOY_CUR_OUT_PATH="${__DEPLOY_OUT_PATH}/$(date '+%Y-%m-%d_%H-%M-%S-%3N')"
 
-	cdepl_cluster_login_cmd "mkdir -p $DEPLOY_CUR_OUT_PATH"
+	cdepl_cluster_login_cmd "mkdir -p $__DEPLOY_CUR_OUT_PATH"
 
-	util_log "[deploy] Deployment output path: $DEPLOY_CUR_OUT_PATH"
+	util_log "[deploy] Deployment output path: $__DEPLOY_CUR_OUT_PATH"
 }
 
 cdepl_deploy_loop_endless()
