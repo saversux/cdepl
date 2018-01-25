b894080ce8ce1d3b102796f927bfaaa6adc0e05e
Stefan Nothaas <stefan.nothaas@hhu.de>
2018-01-25 15:31:00 +0100

    deploy: Refactor output path setup to use cluster depending impl

--- deploy.sh
+++ deploy.sh
@@ -4,27 +4,46 @@ __DEPLOY_OUT_PATH=""
 __DEPLOY_CUR_OUT_PATH=""
 
 ##
-# Set the output path for configuration files, log files, job scripts etc
-# that are created during deployment. Ensure that the location is accessible/
-# mounted on all cluster nodes.
+# Setup the output path for configuration files, log files, job scripts etc
+# that are created during deployment. This location depends on the cluster
+# implementation but uses the current user to put it either into his home
+# directory or somewhere accessable.
 #
-# $1: Full absolute path to the output location on the cluster
+# $1: User name of the account to use on the target cluster
+# $2: Optional, name of the directory to put all output files to (defaults to 
+#     cdepl_out)
 ##
-cdepl_deploy_out_path()
+cdepl_deploy_setup_out_path()
 {
-	local path=$1
+	local user=$1
+	local subdir_name=$2
 
 	# Use the deploy script name for the out folder
 	local filename=$(basename "${BASH_SOURCE[1]}")
 	local extension="${filename##*.}"
 	filename="${filename%.*}"
 
-	__DEPLOY_OUT_PATH="$path"
+	if [ ! "$subdir_name" ]; then
+		subdir_name="cdepl_out"
+	fi
+
+	__DEPLOY_OUT_PATH="$(cdepl_cluster_get_base_path_deploy_out $user)/$subdir_name"
 	__DEPLOY_CUR_OUT_PATH="${__DEPLOY_OUT_PATH}/${filename}_$(date '+%Y-%m-%d_%H-%M-%S-%3N')"
 
 	cdepl_cluster_file_system_cmd "mkdir -p $__DEPLOY_CUR_OUT_PATH"
 
 	util_log "[deploy] Deployment output path: $__DEPLOY_CUR_OUT_PATH"
+}
+
+##
+# Get the currently set out path for logfiles, configs etc of the deployed
+# applications
+#
+# Return stdout: Current path set
+##
+cdepl_deploy_get_out_path()
+{
+	echo "$__DEPLOY_OUT_PATH"
 }
 
 ##
